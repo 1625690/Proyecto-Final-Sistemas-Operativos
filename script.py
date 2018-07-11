06282815a72b45e3ed6ae2777627f21ae6351ed4
Cristian Perafan <crystian.perafan@correounivalle.edu.co>
2018-07-10 23:13:29 -0500

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import sys
 import Adafruit_DHT
 import urllib2
 
-url = 'http://api.thingspeak.com/update?api_key=45UDHEIP7B0HDCJS&field1='
+url = 'https://api.thingspeak.com/update?api_key=RYORGFJXS5TGVH4V&field1='
 
 humedad, temperatura = Adafruit_DHT.read_retry(11, 4)
 print 'Temp: {0:0.1f} C  Humed: {1:0.1f} %'.format(temperatura, humedad)
